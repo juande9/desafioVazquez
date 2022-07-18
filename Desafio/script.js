ee7e319fc7ebde9732f6e2137bc64e753712239d
Juan Manuel <juanmavazquez91@gmail.com>
2022-07-18 09:54:27 -0300

    Corrección Desafio

--- Desafio/script.js
+++ Desafio/script.js
@@ -11,29 +11,33 @@ function validarNumero(solicitud) {
     } return num
 }
 
-/**
- * Inicia la cadena de funciones de calculos.
- * Lo que se ingresa acá, es la cantidad de terminos que se quieren testear.
- */
-function inicioCircuito() {
-    let cant = validarNumero("la cantidad de divisores que se quieren testear.");
-    for (i = 0; i < cant; i++) {
-        corroborarDivision();
-    }
-}
+// /**
+//  * Inicia la cadena de funciones de calculos.
+//  * Lo que se ingresa acá, es la cantidad de terminos que se quieren testear.
+//  */
+// function inicioCircuito() {
+//     let cant = validarNumero("la cantidad de divisores que se quieren testear.");
+//     for (i = 0; i < cant; i++) {
+//         corroborarDivision();
+//     }
+// }
 
 /**
  * Valida si la división entre los números es posible.
  * De ser el dividendo menor que el divisor, por defecto el resultado va a ser siempre NO DIVISIBLE.
  */
 function corroborarDivision() {
-    let divisor = validarNumero("el divisor.");
-    if (dividendo >= divisor) {
-        corroborarDivisor(divisor);
-    }
-    else {
-        alert("El dividendo es menor al divisor.\nEs por eso que " + dividendo + " NO es divisible por " + divisor + "\nIntenta con otro número");
-        console.log("El dividendo es menor al divisor. Es por eso que " + dividendo + " NO es divisible por " + divisor + "\nIntenta con otro número")
+    let cantidadNum = validarNumero("la cantidad de divisores que se quieren testear.")
+    //    let divisor = validarNumero("el divisor.");
+    for (i = 0; i < cantidadNum; i++) {
+        let divisor = validarNumero("el divisor.");
+        if (dividendo >= divisor) {
+            corroborarDivisor(divisor);
+        }
+        else {
+            alert("El dividendo es menor al divisor.\nEs por eso que " + dividendo + " NO es divisible por " + divisor + "\nIntenta con otro número");
+            console.log("El dividendo es menor al divisor. Es por eso que " + dividendo + " NO es divisible por " + divisor + "\nIntenta con otro número")
+        }
     }
 }
 
@@ -56,4 +60,5 @@ function corroborarDivisor(divisorComprobado) {
 }
 
 let dividendo = validarNumero("el dividendo.");
-inicioCircuito();
+//let cantidadNum = validarNumero("la cantidad de divisores que se quieren testear.")
+corroborarDivision();
