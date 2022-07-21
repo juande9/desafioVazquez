affbc0774eb7edf96f7251f252ce4fef40d9aefc
Juan Manuel <juanmavazquez91@gmail.com>
2022-07-21 20:21:22 -0300

    Desafio Array Complementario

--- Desafio/script.js
+++ Desafio/script.js
@@ -1,65 +1,62 @@
-/**
- * Se encarga de validar que lo ingresado por el usuario sea un número entero.
- * @param {string}  solicitud Es lo que se le pide al usuario (dividendo y/o divisor)
- * @returns (dividendo/divisor) Si el número es correcto, lo valida y lo devuelve como variable para continuar con la operación.
- */
-function validarNumero(solicitud) {
-    let num = parseInt(prompt("Ingrese un número entero para usar como " + solicitud));
-    while (isNaN(num)) {
-        alert("El número es incorrecto.");
-        num = parseInt(prompt("Ingrese un número entero para usar como " + solicitud));
-    }
-    return num
+ValortiposDeCambio = []
+NombreCambio = ["Oficial", "Blue", "Cripto", "CCL"]
+
+function cotizacion(NombreCambio) {
+    ValortiposDeCambio.push(parseFloat(prompt("Ingrese cotización: " + NombreCambio)))
 }
 
-// /**
-//  * Inicia la cadena de funciones de calculos.
-//  * Lo que se ingresa acá, es la cantidad de terminos que se quieren testear.
-//  */
-// function inicioCircuito() {
-//     let cant = validarNumero("la cantidad de divisores que se quieren testear.");
-//     for (i = 0; i < cant; i++) {
-//         corroborarDivision();
-//     }
-// }
-
-/**
- * Valida si la división entre los números es posible.
- * De ser el dividendo menor que el divisor, por defecto el resultado va a ser siempre NO DIVISIBLE.
- */
-function corroborarDivision() {
-    let cantidadNum = validarNumero("la cantidad de divisores que se quieren testear.")
-    //    let divisor = validarNumero("el divisor.");
-    for (i = 0; i < cantidadNum; i++) {
-        let divisor = validarNumero("el divisor.");
-        if (dividendo >= divisor) {
-            corroborarDivisor(divisor);
-        }
-        else {
-            alert("El dividendo es menor al divisor.\nEs por eso que " + dividendo + " NO es divisible por " + divisor + "\nIntenta con otro número");
-            console.log("El dividendo es menor al divisor. Es por eso que " + dividendo + " NO es divisible por " + divisor + "\nIntenta con otro número")
-        }
+function cotizarDolar(pesos) {
+    let cotizacion = prompt("¿Que dolar desea tomar?\n1) Oficial\n2) BLUE\n3) Cripto\n4) CCL")
+    switch (cotizacion) {
+        case "1":
+            alert((pesos / ValortiposDeCambio[0]).toFixed(2) + " USD")
+            break;
+        case "2":
+            alert((pesos / ValortiposDeCambio[1]).toFixed(2) + " USD")
+            break;
+        case "3":
+            alert((pesos / ValortiposDeCambio[2]).toFixed(2) + " USD")
+            break;
+        case "4":
+            alert((pesos / ValortiposDeCambio[3]).toFixed(2) + " USD")
+            break;
+        default:
+            alert("Vuelva a ingresar el tipo de cambio que desea tomar")
     }
 }
 
-/**
- * Valida si el divisor y el dividendo son divisibles entre si.
- * Calcula el residuo y si este da 0, los numeros son divisibles entre si.
- * @param {number} divisorComprobado Devuelve el número de cada divisor previamente comprobado.
- */
-function corroborarDivisor(divisorComprobado) {
-    let resultado = dividendo / divisorComprobado;
-    let residuo = dividendo - ((parseInt(resultado)) * divisorComprobado);
-
-    if (residuo == 0) {
-        alert("El número " + dividendo + " es divisible por " + divisorComprobado);
-        console.log("El número " + dividendo + " es divisible por " + divisorComprobado);
-    } else {
-        alert("El número " + dividendo + " NO es divisible por " + divisorComprobado + ".\nSu residuo no da 0, da " + residuo)
-        console.log("El número " + dividendo + " NO es divisible por " + divisorComprobado + ".\nSu residuo no da 0, da " + residuo);
+function cotizarPesos(dolar) {
+    let cotizacion = prompt("¿Que dolar desea tomar?\n1) Oficial\n2) BLUE\n3) Cripto\n4) CCL")
+    switch (cotizacion) {
+        case "1":
+            alert((dolar * ValortiposDeCambio[0]).toFixed(2) + " $")
+            break;
+        case "2":
+            alert((dolar * ValortiposDeCambio[1]).toFixed(2) + " $")
+            break;
+        case "3":
+            alert((dolar * ValortiposDeCambio[2]).toFixed(2) + " $")
+            break;
+        case "4":
+            alert((dolar * ValortiposDeCambio[3]).toFixed(2) + " $")
+            break;
+        default:
+            alert("Vuelva a ingresar el tipo de cambio que desea tomar")
     }
 }
 
-let dividendo = validarNumero("el dividendo.");
-//let cantidadNum = validarNumero("la cantidad de divisores que se quieren testear.")
-corroborarDivision();
+for (i = 0; i < 4; i++) {
+    cotizacion(NombreCambio[i]);
+}
+
+let opcion = prompt("Ingrese 1 si desea convertir pesos a USD.\nIngrese 2 si desea convertir USD a pesos.\nIngrese cualquier otro caracter para salir.")
+switch (opcion) {
+    case "1":
+        cotizarDolar(parseFloat(prompt("Ingrese la cantidad de pesos que desea convertir a USD.")))
+        break;
+    case "2":
+        cotizarPesos(parseFloat(prompt("Ingrese la cantidad de dolares que desea convertir a Pesos.")))
+        break;
+    default:
+        alert("Salio del conversor. Presione F5 para volver a activarlo.")
+}
